4f2605f7f460774b84b47b801700a67bc9b35d8f
Spencer Ng <spenceng@amazon.com>
2024-03-05 16:47:38 -0800

    Updated Copyright statement

--- patched-vscode/extensions/sagemaker-extension/extension.webpack.config.js
+++ patched-vscode/extensions/sagemaker-extension/extension.webpack.config.js
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Copyright Amazon.com Inc. or its affiliates. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
