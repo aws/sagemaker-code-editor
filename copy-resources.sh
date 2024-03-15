571a792e13d9df4bf36e094846053236c9e0e8fb
Spencer Ng <spenceng@amazon.com>
2024-03-15 15:16:48 -0700

    Updated script to handle replacing files that already exist for the same name

--- copy-resources.sh
+++ copy-resources.sh
@@ -9,15 +9,15 @@ DEST_DIR="vscode"
 # Define paths for each file relative to the base directories
 # Format: "source_file_path:destination_file_path"
 FILES_TO_COPY=(
-    "favicon.ico:src/sagemaker-code-editor/vscode/resources/server/favicon.ico"
-    "code-icon.svg:src/sagemaker-code-editor/vscode/src/vs/workbench/browser/media/code-icon.svg"
-    "letterpress-dark.svg:src/sagemaker-code-editor/vscode/src/vs/workbench/browser/parts/editor/media/letterpress-dark.svg"
-    "letterpress-hcDark.svg:src/sagemaker-code-editor/vscode/src/vs/workbench/browser/parts/editor/media/letterpress-hcDark.svg"
-    "letterpress-hcLight.svg:src/sagemaker-code-editor/vscode/src/vs/workbench/browser/parts/editor/media/letterpress-hcLight.svg"
-    "letterpress-light.svg:src/sagemaker-code-editor/vscode/src/vs/workbench/browser/parts/editor/media/letterpress-light.svg"
+    "favicon.ico:resources/server/favicon.ico"
+    "code-icon.svg:src/vs/workbench/browser/media/code-icon.svg"
+    "letterpress-dark.svg:src/vs/workbench/browser/parts/editor/media/letterpress-dark.svg"
+    "letterpress-hcDark.svg:src/vs/workbench/browser/parts/editor/media/letterpress-hcDark.svg"
+    "letterpress-hcLight.svg:src/vs/workbench/browser/parts/editor/media/letterpress-hcLight.svg"
+    "letterpress-light.svg:src/vs/workbench/browser/parts/editor/media/letterpress-light.svg"
 )
 
-# Loop through the file paths and copy each one to its new location
+# Loop through the file paths, check if file exists, and copy each one to its new location
 for FILE_PATH in "${FILES_TO_COPY[@]}"; do
     IFS=":" read -r SRC_FILE DEST_FILE <<< "$FILE_PATH"
     
@@ -25,7 +25,18 @@ for FILE_PATH in "${FILES_TO_COPY[@]}"; do
     FULL_SRC_PATH="$SOURCE_DIR/$SRC_FILE"
     FULL_DEST_PATH="$DEST_DIR/$DEST_FILE"
     
+    # Check if the source file exists
+    if [ ! -f "$FULL_SRC_PATH" ]; then
+        echo "Source file $FULL_SRC_PATH does not exist. Skipping..."
+        continue
+    fi
+
+    # Check if the destination file exists. If so, delete it before copying.
+    if [ -f "$FULL_DEST_PATH" ]; then
+        rm "$FULL_DEST_PATH"
+        echo "Existing file $FULL_DEST_PATH deleted."
+    fi
+    
     # Copy file from source to destination
-    cp "$FULL_SRC_PATH" "$FULL_DEST_PATH"
-    echo "Copied $FULL_SRC_PATH to $FULL_DEST_PATH"
+    cp -v "$FULL_SRC_PATH" "$FULL_DEST_PATH"
 done
